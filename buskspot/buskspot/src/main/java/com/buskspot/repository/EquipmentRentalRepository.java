6be478ca5bb6061988c0989ca9773f1233a06361
dusdnd301 <164764613+dusdnd301@users.noreply.github.com>
2025-06-16 01:39:39 +0900

    Delete buskspot/buskspot directory

--- buskspot/buskspot/src/main/java/com/buskspot/repository/EquipmentRentalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.buskspot.repository;
-
-import com.buskspot.domain.EquipmentRental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipmentRentalRepository extends JpaRepository<EquipmentRental, Long> {
-}
--- buskspot/buskspot/src/main/java/com/buskspot/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.buskspot.repository;
-
-import com.buskspot.domain.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findByPerformerEmail(String email);
-}
